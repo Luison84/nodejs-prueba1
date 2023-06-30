1ba178cd74e7bbcbeca7500266454b5689186c27
Luison84 <luislozano.arica@gmail.com>
2023-06-29 19:03:25 -0500

    third commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
         "msg" : "Ruta Principal"
     })
 })
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor iniciado");
 })
